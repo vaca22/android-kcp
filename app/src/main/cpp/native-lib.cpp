98f82cde2073c10f1e99a84269de768b9259dacd
liguanghao <liguanghao@viatomtech.com>
2022-08-26 16:54:20 +0800

    cvbc

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,96 +7,48 @@
 #include <fcntl.h>
 #include <android/log.h>
 #include <unistd.h>
+#include "ikcp.h"
 
-#define TAG "tcomm"
+#define TAG "gaga"
 #define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__)
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 
-static int sockfd;
 
+int udp_output(const char *buf, int len, ikcpcb *kcp, void *user) {
+    union {
+        int id;
+        void *ptr;
+    } parameter;
+    parameter.ptr = user;
 
+    LOGE("%d", len);
 
-
-void createSocket(){
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        LOGE("create socket error\n");
-    }else{
-        LOGE("fuck2\n");
-    }
-}
-
-
-
-void connectSocket( char *ipaddr,int port){
-    struct sockaddr_in servaddr;
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(port);
-    if (inet_pton(AF_INET, ipaddr, &servaddr.sin_addr) <= 0) {
-        LOGE("inet_pton error for\n");
-        return;
-    }
-    LOGV("connect...");
-    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-        LOGE("connect error\n");
-        return;
-    }
-    LOGV("connect succeed!!!");
-}
-
-
-int  receiveData(int n){
-    if (!sockfd) {
-        LOGE("socket is failed!");
-        return 0;
-    }
-    jbyte buf[n];
-    int rec_len;
-    if ((rec_len = (int) recv(sockfd, buf, sizeof(buf), 0)) == -1) {
-        LOGE("recv error");
-        return 0;
-    }else{
-        LOGE("fuck  %s",buf);
-    }
-    return rec_len;
-}
-
-void sendData(char *data){
-    if (!sockfd) {
-        printf("socket is failed! \n");
-        return;
-    }
-
-    if (send(sockfd, data, strlen(data), 0) < 0) {
-
-    }
-}
-
-void closeSocket(){
-    if (!sockfd)
-        return;
-//    shutdown(sockfd,0);
-    close(sockfd);
+    return 0;
 }
 
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_vaca_myapplication_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-
+        JNIEnv *env,
+        jobject) {
 
 
     LOGE("fuck\n");
-    createSocket();
-    connectSocket("192.168.5.101",12345);
-    receiveData(24);
+    ikcpcb *kcp1 = ikcp_create(0x11223344, (void *) 0);
+    kcp1->output = udp_output;
+    ikcp_wndsize(kcp1, 128, 128);
+    ikcp_nodelay(kcp1, 2, 10, 2, 1);
+    kcp1->rx_minrto = 10;
+    kcp1->fastresend = 1;
 
+    char buffer[20000];
+//    for(int k=0;k<8;k++){
+    ikcp_send(kcp1, buffer, 20000);
 
+//    }
 
-
-
+    ikcp_update(kcp1, 0);
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 

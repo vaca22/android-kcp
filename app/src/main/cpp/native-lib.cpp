44e8b1f56829d4d7ee914470bfdfb55a9f1ed7b9
vaca22 <wsmslgh@gmail.com>
2023-04-29 22:38:34 +0800

    thfghjdffdg

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,94 +7,73 @@
 #include <fcntl.h>
 #include <android/log.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define TAG "tcomm"
 #define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__)
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
-
+#define printf(...) __android_log_print(ANDROID_LOG_ERROR, "vaca", __VA_ARGS__);
 static int sockfd;
 
 
-
-
-void createSocket(){
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+void createSocket() {
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         LOGE("create socket error\n");
-    }else{
-        LOGE("fuck2\n");
+    } else {
+        printf("fuck2\n");
     }
+//    struct sockaddr_in sin;
+//    memset(&sin, 0, sizeof(sin));
+//    sin.sin_family = AF_INET;
+//    sin.sin_addr.s_addr = INADDR_ANY;
+//    sin.sin_port = htons(14977);
+//    bind(sockfd, (struct sockaddr *) &sin, sizeof(sin));
 }
 
 
-
-void connectSocket( char *ipaddr,int port){
-    struct sockaddr_in servaddr;
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(port);
-    if (inet_pton(AF_INET, ipaddr, &servaddr.sin_addr) <= 0) {
-        LOGE("inet_pton error for\n");
-        return;
-    }
-    LOGV("connect...");
-    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-        LOGE("connect error\n");
-        return;
-    }
-    LOGV("connect succeed!!!");
-}
+[[noreturn]] void beat_heart() {
+    while (true) {
+        struct sockaddr_in server_sin;
+        socklen_t server_sz = sizeof(server_sin);
+        char buf[4] = {'a', 'b', 'c', 'd'};
+        char buf2[4] = {'a', 'b', 'c', 'd'};
 
 
-int  receiveData(int n){
-    if (!sockfd) {
-        LOGE("socket is failed!");
-        return 0;
-    }
-    jbyte buf[n];
-    int rec_len;
-    if ((rec_len = (int) recv(sockfd, buf, sizeof(buf), 0)) == -1) {
-        LOGE("recv error");
-        return 0;
-    }else{
-        LOGE("fuck  %s",buf);
-    }
-    return rec_len;
-}
-
-void sendData(char *data){
-    if (!sockfd) {
-        printf("socket is failed! \n");
-        return;
-    }
-
-    if (send(sockfd, data, strlen(data), 0) < 0) {
-
+        recvfrom(sockfd, &buf, sizeof(buf) - 1, 0, (struct sockaddr *) &server_sin, &server_sz);
+        printf("%s", buf);
     }
 }
 
-void closeSocket(){
-    if (!sockfd)
-        return;
-//    shutdown(sockfd,0);
-    close(sockfd);
-}
+void beat_heart2() {
+    usleep(2000);
 
+    struct sockaddr_in dest_addr;
+    dest_addr.sin_addr.s_addr = inet_addr("192.168.6.14");
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(14971);
+    char buf2[]="fuckyou";
+
+    sendto(sockfd, buf2, strlen(buf2), 0,(struct sockaddr *)&dest_addr, sizeof(dest_addr));
+
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_vaca_myapplication_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
 
 
-
-    LOGE("fuck\n");
     createSocket();
-    connectSocket("192.168.5.101",12345);
-    receiveData(24);
+    pthread_t thread1;
+    pthread_t thread2;
+    char *message1 = "Thread 1";
+    pthread_create(&thread1, nullptr, reinterpret_cast<void *(*)(void *)>(beat_heart),
+                   (void *) message1);
 
-
-
+    char *message2 = "Thread 2";
+    pthread_create(&thread2, nullptr, reinterpret_cast<void *(*)(void *)>(beat_heart2),
+                   (void *) message2);
 
 
     std::string hello = "Hello from C++";
